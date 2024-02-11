d59dfc30a2da662d816e078db70d74a61aa2a8cc
rmac3392 <ryanjamesmacawili@gmail.com>
2024-02-12 07:55:22 +0800

    first commoot

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Arduino Control</h1>
+    <button @click="turnOn">Turn On</button>
+    <button @click="turnOff">Turn Off</button>
+  </div>
+</template>
+
+<script setup>
+import io from "socket.io-client";
+
+const socket = io('http://localhost:3000');
+//const socket = io("http://localhost:5173");
+
+const turnOn = () => {
+  console.log("Connect to server on");
+  socket.emit("on", 13);
+};
+const turnOff = () => {
+  console.log("Connect to server off");
+  socket.emit("off", 13);
+};
+</script>
