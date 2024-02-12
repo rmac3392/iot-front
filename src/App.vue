06b615c786ee2f6d3b17249ed5842bcb6954c7e8
rmac3392 <ryanjamesmacawili@gmail.com>
2024-02-12 09:42:22 +0800

    change server

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <script setup>
 import io from "socket.io-client";
 
-const socket = io('http://localhost:3000');
+const socket = io('https://iot-elyc.onrender.com/');
 //const socket = io("http://localhost:5173");
 
 const turnOn = () => {
